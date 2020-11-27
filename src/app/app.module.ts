09fb4319e54345ca3aa185329a6e1f37a4798fe8
Sergio Carralero <sergiocn.dev@gmail.com>
2020-11-27 21:02:57 +0100

    Ended section 4 - Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Modules
+import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
+
 import { AppComponent } from './app.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, NopagefoundComponent],
+  imports: [BrowserModule, AppRoutingModule, PagesModule, AuthModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
