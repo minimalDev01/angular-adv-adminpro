0a63eb1b7b1d6f9e9bdeb4b775cf093bc6cad58d
Sergio Carralero <sergiocn.dev@gmail.com>
2020-12-14 14:42:27 +0100

    Environment prod ready

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -4,15 +4,37 @@ import {
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
   Router,
+  CanLoad,
+  Route,
+  UrlSegment,
+  UrlTree,
 } from '@angular/router';
 import { UserService } from '../services/user.service';
 import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private userService: UserService, private router: Router) {}
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.userService.validateToken().pipe(
+      tap((isAuth) => {
+        if (!isAuth) {
+          this.router.navigateByUrl('/login');
+        }
+      })
+    );
+  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     return this.userService.validateToken().pipe(
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -4,84 +4,15 @@ import { NgModule } from '@angular/core';
 import { AuthGuard } from '../guards/auth.guard';
 
 import { PagesComponent } from './pages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProgressComponent } from './progress/progress.component';
-import { Grafica1Component } from './grafica1/grafica1.component';
-import { AccountSettingsComponent } from './account-settings/account-settings.component';
-import { PromisesComponent } from './promises/promises.component';
-import { RxjsComponent } from './rxjs/rxjs.component';
-import { ProfileComponent } from './profile/profile.component';
-
-// Maintenances
-import { UsersComponent } from './maintenances/users/users.component';
-import { HospitalsComponent } from './maintenances/hospitals/hospitals.component';
-import { MedicsComponent } from './maintenances/medics/medics.component';
-import { MedicComponent } from './maintenances/medics/medic.component';
-import { SearchComponent } from './search/search.component';
-import { AdminGuard } from '../guards/admin.guard';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: PagesComponent,
     canActivate: [AuthGuard],
-    children: [
-      { path: '', component: DashboardComponent, data: { title: 'Dashboard' } },
-      {
-        path: 'account-settings',
-        component: AccountSettingsComponent,
-        data: { title: 'Account Settings' },
-      },
-      {
-        path: 'search/:term',
-        component: SearchComponent,
-        data: { title: 'Searches' },
-      },
-      {
-        path: 'progress',
-        component: ProgressComponent,
-        data: { title: 'Progress Bar' },
-      },
-      {
-        path: 'grafica1',
-        component: Grafica1Component,
-        data: { title: 'Graphics' },
-      },
-      {
-        path: 'promises',
-        component: PromisesComponent,
-        data: { title: 'Promises' },
-      },
-      { path: 'rxjs', component: RxjsComponent, data: { title: 'RxJs' } },
-      {
-        path: 'profile',
-        component: ProfileComponent,
-        data: { title: 'User Profile' },
-      },
-
-      // Maintenances
-      {
-        path: 'users',
-        canActivate: [AdminGuard],
-        component: UsersComponent,
-        data: { title: 'Users Maintenances' },
-      },
-      {
-        path: 'hospitals',
-        component: HospitalsComponent,
-        data: { title: 'Hospitals Maintenances' },
-      },
-      {
-        path: 'medics',
-        component: MedicsComponent,
-        data: { title: 'Medics Maintenances' },
-      },
-      {
-        path: 'medic/:id',
-        component: MedicComponent,
-        data: { title: 'Medic Maintenances' },
-      },
-    ],
+    canLoad: [AuthGuard],
+    loadChildren: () =>
+      import('./child-routes.module').then((m) => m.ChildRoutesModule),
   },
 ];
 
